ad8328861e258a9b939268a955dc56f8a26bd199
selva <selvapandy.m@gmail.com>
2020-09-06 14:42:50 -0400

    jwt-springsecured-gateway - "First commit"

---
+++ gateway-secure-service/src/main/java/com/gateway/service/ILoginService.java
@@ -0,0 +1,14 @@
+package com.gateway.service;
+
+import com.gateway.bean.User;
+
+public interface ILoginService {
+    String login(String username, String password);
+    User saveUser(User user);
+
+    boolean logout(String token);
+
+    Boolean isValidToken(String token);
+
+    String createNewToken(String token);
+}
